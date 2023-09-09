1495d3625435c00da5e220c4404e9699d76d50f3
Adam Drake <adam@chemify.io>
2023-09-09 15:22:23 +0200

    traits traits traits!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod shapes;
+
+use crate::shapes::{rect::Rect, circle::Circle, area::Area};
+
+
 fn main() {
-  
+  let rect = Rect::default();
+  let circ = Circle {
+    x: 0.0,
+    y: 0.0,
+    radius: 10.0
+  };
+  println!("{}", rect);
+  println!("{}", circ);
 }
