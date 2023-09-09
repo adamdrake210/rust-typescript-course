eb04400604bb4d9f82d375749c6d891c4ffa294d
Adam Drake <adam@chemify.io>
2023-09-09 12:51:29 +0200

    updates for borrower checker

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-#[derive(Debug)]
-struct Item {
-  count: usize
-}
-
-fn add_one(item: &mut Item) {
-  item.count += 1;
-}
-
 fn main() {
-  let mut item = Item { count: 1 };
-  println!("{:?}", item);
-  add_one(&mut item);
-  println!("{:?}", item);
+  
 }
-
-// Questions
-// - Who owns the value?
-// - How long does this value live?
-// There can only be one owner. There can only be one lord of the ring
