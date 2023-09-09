1144051f90ede165999873edf489da84e7579357
Adam Drake <adam@chemify.io>
2023-09-09 11:31:38 +0200

    updating main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
-// fn multiply(nums: Vec<usize>, index: usize) -> usize {
-//   return nums.get(index).unwrap_or(&index) * 5;
-// }
+#[derive(Debug)]
+struct Item {
+  count: usize
+}
 
-// fn main() {
-
-//   let vec = vec![1, 2, 4, 5, 6];
-
-//   println!("{}", multiply(vec, 0))
-
-// }
-
-// Rust Error Libraries
-// thiserror
-// anyhow - for applications
+fn add_one(item: &mut Item) {
+  item.count += 1;
+}
 
 fn main() {
-  let file_name = std::env::args().nth(1)
-      .expect("the file name to be passed in");
-
-  let file = std::fs::read_to_string(file_name)
-      .expect("unable to read file contents");
-
-  file.lines().for_each(|line| println!("{}", line));
-  
+  let mut item = Item { count: 1 };
+  println!("{:?}", item);
+  add_one(&mut item);
+  println!("{:?}", item);
 }
+
+// Questions
+// - Who owns the value?
+// - How long does this value live?
+// There can only be one owner. There can only be one lord of the ring
